3b227c8a0840f389307136bdf6a27b7a1f4f7bc5
Anıl Çağlayan <huseyin.caglayan@stu.fbu.edu.tr>
2025-07-10 14:20:28 +0300

    Add files via upload

---
+++ ANİL8.py
@@ -0,0 +1,13 @@
+companyA = 15000
+companyB = 25000
+
+counter = 0
+
+while companyA < companyB:
+    companyA = companyA * 1.06
+    companyB = companyB * 1.02
+    counter +=1
+    
+print(counter)
+    
+    
